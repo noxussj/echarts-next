166d673f07a7d2bd4481800ce05cf678be093cd3
HN <632922356@qq.com>
2022-10-25 09:52:08 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { loadStyle } from './core/echarts-style';
+import _echarts from './utils/echarts-register';
 import BarxSimple from './components/barx/barx-simple';
 import BarxMultiple from './components/barx/barx-multiple';
 import BarxStack from './components/barx/barx-stack';
@@ -10,4 +11,4 @@ import BaryStack from './components/bary/bary-stack';
 import LineSimple from './components/line/line-simple';
 import PieSimple from './components/pie/pie-simple';
 
-export { loadStyle, BarxSimple, BarxMultiple, BarxStack, BarySimple, BaryRank, BaryRank2, BaryMultiple, BaryStack, LineSimple, PieSimple };
+export { loadStyle, _echarts, BarxSimple, BarxMultiple, BarxStack, BarySimple, BaryRank, BaryRank2, BaryMultiple, BaryStack, LineSimple, PieSimple };
--- utils/echarts-register.ts
+++ utils/echarts-register.ts
@@ -1,6 +1,16 @@
 import * as echarts from 'echarts';
 
 class Echarts {
+    loaded: any[];
+
+    constructor() {
+        this.loaded = [];
+
+        window.addEventListener('resize', () => {
+            this.resize();
+        });
+    }
+
     /**
      * 渲染图表
      */
@@ -11,7 +21,33 @@ class Echarts {
 
         instance.setOption(opt);
 
+        this.loaded.push(instance);
+
         return instance;
+    }
+
+    /**
+     * 图表重载
+     */
+    resize() {
+        this.loaded.forEach((instance: any) => {
+            setTimeout(() => {
+                instance.resize();
+            }, 0);
+        });
+    }
+
+    /**
+     * 销毁图表
+     */
+    destroy() {
+        while (this.loaded.length) {
+            const instance = this.loaded.pop();
+
+            setTimeout(() => {
+                instance.dispose();
+            }, 0);
+        }
     }
 }
 
