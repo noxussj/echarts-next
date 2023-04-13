d33d03bf163e8548e139aaf7fab0c839db64db04
HN <632922356@qq.com>
2023-04-13 11:38:22 +0800

    update

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import _echarts from './utils/echarts-register';
 import BarxSimple from './components/barx/barx-simple';
 import BarxMultiple from './components/barx/barx-multiple';
 import BarxStack from './components/barx/barx-stack';
+import BaryDynamic from './components/bary/bary-dynamic';
 import BarySimple from './components/bary/bary-simple';
 import BaryRank from './components/bary/bary-rank';
 import BaryRank2 from './components/bary/bary-rank2';
@@ -12,4 +13,19 @@ import LineSimple from './components/line/line-simple';
 import PieSimple from './components/pie/pie-simple';
 import RadarBlock from './components/radar/radar-block';
 
-export { loadStyle, _echarts, BarxSimple, BarxMultiple, BarxStack, BarySimple, BaryRank, BaryRank2, BaryMultiple, BaryStack, LineSimple, PieSimple, RadarBlock };
+export {
+    loadStyle,
+    _echarts,
+    BarxSimple,
+    BarxMultiple,
+    BarxStack,
+    BaryDynamic,
+    BarySimple,
+    BaryRank,
+    BaryRank2,
+    BaryMultiple,
+    BaryStack,
+    LineSimple,
+    PieSimple,
+    RadarBlock,
+};
