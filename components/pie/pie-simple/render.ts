c7bdfff5b95310cac8dfecc231ba228c69e9df04
noxussj <632922356@qq.com>
2023-12-06 16:35:07 +0800

    update

--- components/pie/pie-simple/render.ts
+++ components/pie/pie-simple/render.ts
@@ -2,6 +2,11 @@ import _echarts from '../../../utils/echarts-register';
 import { extens } from '../../../core/echarts-extens';
 import { useStyle } from '../../../core/echarts-style';
 
+const convertToRem = (pxValue: number) => {
+    const remValue = pxValue / 16; // 假设根元素字体大小为16px
+    return `${remValue}rem`;
+};
+
 export default ({ $dom, $opt, $data, $seriesColor, $center, $radius }: any) => {
     const { $color, $tooltip } = useStyle();
 
@@ -16,29 +21,37 @@ export default ({ $dom, $opt, $data, $seriesColor, $center, $radius }: any) => {
             center: $center,
             radius: $radius,
             avoidLabelOverlap: false,
+            itemStyle: {
+                borderRadius: 10,
+            },
             label: {
                 show: false,
                 position: 'center',
                 color: '#fff',
             },
             emphasis: {
+                scale: 1,
                 label: {
                     show: true,
                     fontSize: '20',
                     fontWeight: 'bold',
                     formatter: (e: any) => {
-                        let res = `{name|${e.name}}`;
+                        let res = `{value|${e.value}}`;
 
-                        res += `\n{value|${e.value}}`;
+                        res += `\n{name|${e.name} (${e.data.unit})}`;
 
                         return res;
                     },
                     rich: {
                         name: {
-                            lineHeight: 30,
+                            color: '#BBD3FB',
+                            fontSize: convertToRem(11),
+                            lineHeight: 20,
                         },
                         value: {
-                            lineHeight: 30,
+                            color: '#FFFFFF',
+                            fontSize: convertToRem(20),
+                            lineHeight: 20,
                         },
                     },
                 },
@@ -54,6 +67,9 @@ export default ({ $dom, $opt, $data, $seriesColor, $center, $radius }: any) => {
      * 导出配置项
      */
     const options = {
+        textStyle: {
+            fontFamily: 'PingFang SC, Microsoft YaHei, Arial Regular',
+        },
         color: $seriesColor || $color.theme,
         legend: legend,
         tooltip: Object.assign(
@@ -68,5 +84,14 @@ export default ({ $dom, $opt, $data, $seriesColor, $center, $radius }: any) => {
     /**
      * 继承配置项后渲染图表
      */
-    _echarts.render($dom, extens($opt, options));
+    const instance = _echarts.render($dom, extens($opt, options));
+
+    /**
+     * 高亮第一项
+     */
+    instance.dispatchAction({
+        type: 'highlight',
+        seriesIndex: 0,
+        dataIndex: 0,
+    });
 };
